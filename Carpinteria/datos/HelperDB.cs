6211ad56e78c7b2d6af7ac0b95b7fd97247ed36d
113443_fonseca_victoria <113443@tecnicatura.frc.utn.edu.ar>
2023-04-13 16:38:14 -0300

    Proyecto Carpintería

---
+++ Carpinteria/datos/HelperDB.cs
@@ -0,0 +1,119 @@
+﻿using CarpinteriaApp.dominio;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CarpinteriaApp.datos
+{
+    class HelperDB
+    {
+        private SqlConnection conn;
+        private string CadenaConexion = @"Data Source=DESKTOP-64M431K\SQLEXPRESS;Initial Catalog=carpinteria_db;Integrated Security=True";
+        private static HelperDB instancia;
+
+        public HelperDB()
+        {
+            conn = new SqlConnection(CadenaConexion);
+        }
+
+        public static HelperDB ObtenerInstancia()
+        {
+            if (instancia == null)
+
+                instancia = new HelperDB();
+            return instancia;
+
+        }
+
+        public DataTable ConsultarDB(string NomProc)
+        {
+            DataTable tabla = new DataTable();
+            SqlCommand cmd = new SqlCommand();
+            conn.Open();
+            cmd.Connection = conn;
+            cmd.CommandText = NomProc;
+            cmd.CommandType = CommandType.StoredProcedure;
+            tabla.Load(cmd.ExecuteReader());
+            conn.Close();
+            return tabla;
+        }
+
+
+
+        public bool ConfirmarPresupuesto(Presupuesto oPresupuesto)
+        {
+            bool resultado = true;
+
+            SqlConnection cnn = new SqlConnection();
+            SqlTransaction trans = null;
+
+       try {
+
+            cnn.ConnectionString = @"Data Source=DESKTOP-64M431K\SQLEXPRESS;Initial Catalog=carpinteria_db;Integrated Security=True";
+            cnn.Open();
+            trans = cnn.BeginTransaction();
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Transaction = trans;
+            cmd.Connection = cnn;
+            cmd.CommandText = "SP_INSERTAR_MAESTRO";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@cliente", oPresupuesto.Cliente);
+            cmd.Parameters.AddWithValue("@dto", oPresupuesto.Descuento);
+            cmd.Parameters.AddWithValue("@total", oPresupuesto.CalcularTotal());
+
+            //parámetro de salida:
+            SqlParameter pOut = new SqlParameter();
+            pOut.ParameterName = "@presupuesto_nro";
+            pOut.DbType = DbType.Int32;
+            pOut.Direction = ParameterDirection.Output;
+            cmd.Parameters.Add(pOut);
+
+            cmd.ExecuteNonQuery();
+
+            int presupuestoNro = (int)pOut.Value;
+
+                   //si es ident esto no va 
+                int detalleNro = 1; //esto tampoco
+
+                foreach (DetallePresupuesto item in oPresupuesto.Detalles)
+                {
+                    SqlCommand cmdDet = new SqlCommand();
+                    cmdDet.Connection = cnn;
+                    cmdDet.Transaction = trans;
+
+                    cmdDet.CommandText = "SP_INSERTAR_DETALLE";
+                    cmdDet.CommandType = CommandType.StoredProcedure;
+                    cmdDet.Parameters.AddWithValue("@presupuesto_nro", presupuestoNro);
+                    cmdDet.Parameters.AddWithValue("@detalle", detalleNro);  //si es identity no va 
+                    cmdDet.Parameters.AddWithValue("@id_producto", item.Producto.ProductoNro);
+                    cmdDet.Parameters.AddWithValue("@cantidad", item.Cantidad);
+
+                    cmd.ExecuteNonQuery();
+                    detalleNro++;  //si es identity no va 
+                }
+                trans.Commit();
+                cnn.Close();
+                
+            }
+            catch(Exception)
+            {
+
+                trans.Rollback();
+                resultado = false;
+            }
+            finally
+            {
+                if (cnn != null && cnn.State == ConnectionState.Open) cnn.Close();
+            }
+
+            return resultado;
+        }
+    }
+}
+
---
+++ Carpinteria/formularios/Frm_Alta_Presupuesto.cs
@@ -0,0 +1,161 @@
+﻿using CarpinteriaApp.datos;
+using CarpinteriaApp.dominio;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace CarpinteriaApp.formularios
+{
+    public partial class Frm_Alta_Presupuesto : Form
+    {
+        private HelperDB gestor;
+        private Presupuesto nuevo;
+
+        public Frm_Alta_Presupuesto()
+        {
+            InitializeComponent();
+            gestor = new HelperDB();
+            CargarProductos();
+            nuevo = new Presupuesto();
+        }
+
+        private void Frm_Alta_Presupuesto_Load(object sender, EventArgs e)
+        {
+            ProximoPresupuesto();
+            //hacer el sub y cal total 
+            lblNroPresupuesto.Text += " " + ProximoPresupuesto();
+            dtpFecha.Enabled = false;
+        }
+
+        private int ProximoPresupuesto()
+        {
+            SqlConnection conexion = new SqlConnection();
+            conexion.ConnectionString = @"Data Source=DESKTOP-64M431K\SQLEXPRESS;Initial Catalog=carpinteria_db;Integrated Security=True";
+            conexion.Open();
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexion;
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.CommandText = "SP_PROXIMO_ID";
+            SqlParameter param = new SqlParameter("@next", SqlDbType.Int);
+            param.Direction = ParameterDirection.Output;
+            comando.Parameters.Add(param);
+            comando.ExecuteNonQuery();
+            conexion.Close();
+
+            return (int)param.Value;
+        }
+
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (cboProductos.Text.Equals(String.Empty))
+            {
+                MessageBox.Show("Debe seleccionar un PRODUCTO!", "Control", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            if (txtCantidad.Text == "" || !int.TryParse(txtCantidad.Text, out _))
+            {
+                MessageBox.Show("Debe ingresar una cantidad válida!", "Control", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            foreach (DataGridViewRow row in dgvDetalles.Rows)
+            {
+                if (row.Cells["colProd"].Value.ToString().Equals(cboProductos.Text))
+                {
+                    MessageBox.Show("PRODUCTO: " + cboProductos.Text + " ya se encuentra como detalle!", "Control", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return;
+
+                }
+            }
+            DataRowView item = (DataRowView)cboProductos.SelectedItem;
+
+            int prod = Convert.ToInt32(item.Row.ItemArray[0]);
+            string nom = item.Row.ItemArray[1].ToString();
+            double pre = Convert.ToDouble(item.Row.ItemArray[2]);
+            Producto p = new Producto(prod, nom, pre);
+            int cantidad = Convert.ToInt32(txtCantidad.Text);
+
+            DetallePresupuesto detalle = new DetallePresupuesto(p, cantidad);
+            nuevo.AgregarDetalle(detalle);
+            dgvDetalles.Rows.Add(new object[] { item.Row.ItemArray[0], item.Row.ItemArray[1], item.Row.ItemArray[2], txtCantidad.Text });
+
+            CalcularTotal();
+        }
+
+        private void dgvDetalles_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvDetalles.CurrentCell.ColumnIndex == 4)
+            {
+                nuevo.QuitarDetalle(dgvDetalles.CurrentRow.Index);
+                dgvDetalles.Rows.Remove(dgvDetalles.CurrentRow);
+                CalcularTotal();
+            }
+        }
+
+        private void CalcularTotal()
+        {
+            double total = nuevo.CalcularTotal();
+            txtTotal.Text = total.ToString();
+
+            if (txtDto.Text != "")
+            {
+                double dto = (total * Convert.ToDouble(txtDto.Text)) / 100;
+                txtFinal.Text = (total - dto).ToString();
+            }
+
+        }
+
+        private void CargarProductos()  
+        {
+            DataTable table = gestor.ConsultarDB("SP_CONSULTAR_PRODUCTOS");
+            if (table != null)
+            {
+                cboProductos.DataSource = table;
+                cboProductos.DisplayMember = "n_producto";
+                cboProductos.ValueMember = "id_producto";
+            }
+        }
+
+        private void GuardarPresupuesto() //bien 
+        {
+            
+            nuevo.Fecha = Convert.ToDateTime(dtpFecha.Value);
+            nuevo.Cliente = txtCliente.Text; //bien 
+            nuevo.Descuento = Convert.ToDouble(txtDto.Text); //bien 
+ 
+
+            if (gestor.ConfirmarPresupuesto(nuevo))
+            {
+                MessageBox.Show("Presupuesto registrado", "Informe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Dispose();
+            }
+            else
+            {
+                MessageBox.Show("ERROR. No se pudo registrar el presupuesto", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            if (txtCliente.Text == "")
+            {
+                MessageBox.Show("Debe ingresar un cliente!", "Control", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+            if (dgvDetalles.Rows.Count == 0)
+            {
+                MessageBox.Show("Debe ingresar al menos detalle!", "Control", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+            GuardarPresupuesto();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+    }
+}
